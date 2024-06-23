da87406efae8c5b02726e64aa8ac9effce979e5e
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-06-24 00:39:29 +0500

    add loader

--- src/components/task-lists/task-lists.tsx
+++ src/components/task-lists/task-lists.tsx
@@ -1,10 +1,16 @@
 import { useTodos } from '@/services/todos/todos.hooks'
+import { LoaderCircle } from 'lucide-react'
 import { TaskList } from './task-list'
 
 export const TaskLists = () => {
 	const { data: todos, isLoading } = useTodos()
 
-	if (isLoading) return <div>Loading...</div>
+	if (isLoading)
+		return (
+			<div className='flex items-center justify-center'>
+				Loading... <LoaderCircle className='animate-spin' size={16} />
+			</div>
+		)
 
 	if (!todos?.length) return <div className='text-center text-gray-400 text-lg'>No list available. </div>
 
