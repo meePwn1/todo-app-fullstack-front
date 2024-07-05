49f000cf7ecaf881a20078d3134b1589a9862386
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-07-06 03:36:52 +0500

    ref: .env api_key

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,7 +10,7 @@ import axios from 'axios'
 export const api = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_TODO_APP_URL,
 	headers: {
-		'API-KEY': '90ee9bc0-e96b-4a8d-a427-fa62375019aa',
+		'API-KEY': process.env.NEXT_PUBLIC_API_KEY,
 	},
 	withCredentials: true,
 })
